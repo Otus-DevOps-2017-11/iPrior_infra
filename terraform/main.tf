81acd5a5f6bee0c899158f63e98da15c818d2c26
Ivan Priorov <darkmonk9@gmail.com>
2018-01-10 02:43:21 +0300

    Homework 08

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,57 @@
 provider "google" {
-  region = "europe-west1"
+  region  = "${var.region}"
   version = "1.4.0"
-  project = "infra-189212"
+  project = "${var.project}"
 }
 
 resource "google_compute_instance" "app" {
   machine_type = "g1-small"
-  name = "reddit-app"
-  zone = "europe-west1-b"
+  name         = "reddit-app"
+  zone         = "${var.zone}"
+
   "boot_disk" {
     initialize_params {
-      image = "reddit-base"
+      image = "${var.disk_image}"
     }
   }
+
   "network_interface" {
-    network = "default"
-    access_config {}
+    network       = "default"
+    access_config = {}
   }
+
+  tags = ["reddit-app"]
+
+  metadata {
+    sshKeys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
 }
